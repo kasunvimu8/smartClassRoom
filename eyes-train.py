323a17fd0f0fd1321971921bd02be54707e1f648
shehanshaman <shehanshaman@outlook.com>
2018-12-14 00:37:46 +0530

    Adding eye train files

--- eyes-train.py
+++ eyes-train.py
@@ -8,6 +8,8 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 image_dir = os.path.join(BASE_DIR, "images")
 
 face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+eye_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_eye.xml')
+
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 
 current_id = 0
@@ -25,17 +27,15 @@ for root, dirs, files in os.walk(image_dir):
 				label_ids[label] = current_id
 				current_id += 1
 			id_ = label_ids[label]
-			#print(label_ids)
-			#y_labels.append(label) # some number
-			#x_train.append(path) # verify this image, turn into a NUMPY arrray, GRAY
+			
 			pil_image = Image.open(path).convert("L") # grayscale
 			size = (550, 550)
 			final_image = pil_image.resize(size, Image.ANTIALIAS)
 			image_array = np.array(final_image, "uint8")
 			#print(image_array)
-			faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=5)
+			eyes = eye_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=5)
 
-			for (x,y,w,h) in faces:
+			for (x,y,w,h) in eyes:
 				roi = image_array[y:y+h, x:x+w]
 				x_train.append(roi)
 				y_labels.append(id_)
@@ -44,8 +44,8 @@ for root, dirs, files in os.walk(image_dir):
 #print(y_labels)
 #print(x_train)
 
-with open("pickles/face-labels.pickle", 'wb') as f:
+with open("pickles/eye-labels.pickle", 'wb') as f:
 	pickle.dump(label_ids, f)
 
 recognizer.train(x_train, np.array(y_labels))
-recognizer.save("recognizers/face-trainner.yml")
+recognizer.save("recognizers/eye-trainner.yml")
