4a67a0e96739cb78b92dd8cacf1b2ee5738e8c06
kasunvimu8 <kasunvimu8@gmail.com>
2019-02-21 21:28:46 +0530

    change camera frames with pictures

--- eye_face.py
+++ eye_face.py
@@ -44,62 +44,64 @@ def logTranform(img) :
 
 def enhancingImage(gray):
     img=histogramEq(gray)
-    img=logTranform(img);
+   # img=logTranform(img);
     # cv2.imshow('gray',gray)
     # cv2.imshow('gray',img)
     return img
 
-
+i=0
 while(True):
-    time.sleep(0.1)
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    gray  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    processedImg=enhancingImage(gray)
+    i=i+1
+#     time.sleep(0.1)
+#     # Capture frame-by-frame
+#     ret, frame = cap.read()
+    frame =cv2.imread('testing_image/test9.jpg')
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    processedImg = enhancingImage(gray)
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
 
     for (x, y, w, h) in faces:
-        #print(x,y,w,h)
-        roi_gray = gray[y:y+h, x:x+w] #(ycord_start, ycord_end)
-        roi_color = frame[y:y+h, x:x+w]
+            #print(x,y,w,h)
+            roi_gray = gray[y:y+h, x:x+w] #(ycord_start, ycord_end)
+            roi_color = frame[y:y+h, x:x+w]
 
-        # recognize? deep learned model predict keras tensorflow pytorch scikit learn
-        id_, conf = recognizer.predict(roi_gray)
-        id_2, conf_eye = recognizer_eye.predict(roi_gray)
-      #   print(conf , " " , conf_eye)
-        if conf>=40 and conf <= 85 and conf_eye>=100 and conf_eye <= 150:
-            #print(5: #id_)
-            #print(labels[id_])
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            name = labels[id_]
-            name2 = eye_labels[id_2]
-            color = (255, 255, 255)
+            # recognize? deep learned model predict keras tensorflow pytorch scikit learn
+            id_, conf = recognizer.predict(roi_gray)
+            id_2, conf_eye = recognizer_eye.predict(roi_gray)
+            #   print(conf , " " , conf_eye)
+            if conf>=40 and conf <= 85 and conf_eye>=100 and conf_eye <= 150:
+                #print(5: #id_)
+                #print(labels[id_])
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                name = labels[id_]
+                name2 = eye_labels[id_2]
+                color = (255, 255, 255)
+                stroke = 2
+              #  print(name," ",name2)
+                if name == name2:
+                    cv2.putText(frame, name, (x,y-15), font, 0.8, color, stroke, cv2.LINE_AA)
+
+
+            color = (255, 0, 0) #BGR 0-255
             stroke = 2
-          #  print(name," ",name2)
-            if name == name2:
-                cv2.putText(frame, name, (x,y-15), font, 0.8, color, stroke, cv2.LINE_AA)
+            end_cord_x = x + w
+            end_cord_y = y + h
+            cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
 
+            #subitems = smile_cascade.detectMultiScale(roi_gray)
+            #for (ex,ey,ew,eh) in subitems:
+            #   cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
-        color = (255, 0, 0) #BGR 0-255 
-        stroke = 2
-        end_cord_x = x + w
-        end_cord_y = y + h
-        cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
+    if i==1:
+       print(" Number of Students : ", len(faces)) #number of persons
 
-        #subitems = smile_cascade.detectMultiScale(roi_gray)
-        #for (ex,ey,ew,eh) in subitems:
-        #   cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-
-
-    print(" Number of Students : ", len(faces)) #number of persons
-
-    # Display the resulting frame
+        ## Display the resulting frame
     cv2.imshow('frame',frame)
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
 
 # When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+# cap.release()
+# cv2.destroyAllWindows()
 
 
